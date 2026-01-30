a2fcfb1f8365cd34845d45bc8a4fd5b47973ebc8
Falcroft7 <kevin.patry22@gmail.com>
2026-01-30 13:46:49 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,3 @@
+const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5vls80UMNhXVm9NImEGovsuQa3OKc_jnsGqcinv_cTQ6JzolwF7K2Z459Os07qnyXC6hx48M8EW6k/pub?output=csv'; 
 
+let wishlistData = [];
