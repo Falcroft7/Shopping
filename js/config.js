3ea5edf7db9c2a7305521036b84b24aa9d21dc7e
Falcroft7 <kevin.patry22@gmail.com>
2026-01-30 16:57:07 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5vls80UMNhXVm9NImEGovsuQa3OKc_jnsGqcinv_cTQ6JzolwF7K2Z459Os07qnyXC6hx48M8EW6k/pub?output=csv'; 
-
+const SPECIAL_STORE = "Boutique ADA";
 let wishlistData = [];
