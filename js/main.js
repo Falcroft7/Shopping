310ac3fcc6ab2fe1023f815dfce1c38230296e70
Falcroft7 <kevin.patry22@gmail.com>
2026-01-30 13:49:31 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+function init() {
+    Papa.parse(CSV_URL, {
+        download: true,
+        header: true,
+        skipEmptyLines: true,
+        complete: (results) => {
+            wishlistData = results.data;
+            
+            setupFilters();
+            render(wishlistData);
+        }
+    });
+}
 
+init();
